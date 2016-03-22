fc7ba2e9e3425af56f096c4bc7528d41f0643e73
zm025251 <zhangming025251@gmail.com>
2016-03-22 19:08:27 +0800

    tmp

--- c++/string.cpp
+++ c++/string.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
+class A
+{
+    public:
+        operator std::string () const
+        {
+            stringstream stream;
+            stream << "hello\n";
+            //cout << stream.str();
+            return stream.str();
+        }
+};
+
+
 int main(int argc, char *argv[])
 {
-    char * ptr = NULL;
-    //err
-   // string str(ptr);
-   string str("hello world");
-
-    cout << str.c_str();
+    A a;
+    //cout << str.c_str();
+    //a.string();
+    //cout << a;
+    string b = a;
+    cout << b.c_str();
 
     return 0;
 }
