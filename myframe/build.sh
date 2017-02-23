d8091b6330daa3570e77f7f8bde08dcca1aa52d6
zm025251 <zhangming025251@gmail.com>
2017-02-23 16:55:33 +0800

    tmp

--- myframe/build.sh
+++ myframe/build.sh
@@ -7,7 +7,6 @@ rm -rf lib
 mkdir -p lib
 
 
-cd src
 cd idl
 protoc  --cpp_out=. ./myframe.proto
 cd ..
@@ -15,14 +14,14 @@ cd ..
 
 if [ $# == 0 ]; then
     make clean && make
-    mv lib*.a ../lib/
+    mv core/lib*.a lib
 elif [ $# == 1 ];then
     if [ $1 == "clean" ];then
         make clean
-        (cd ../test && make clean)
+        (cd test && make clean)
     elif [ $1 == "all" ]; then
         make clean && make
-        mv lib*.a ../lib/
-        (cd ../test && make clean && make)
+        mv core/lib*.a lib
+        (cd test && make clean && make)
     fi
 fi
