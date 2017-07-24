543bb5867912f77ea3bdccf96b70bb1d17748417
zm025251 <zhangming025251@gmail.com>
2017-07-24 19:07:33 +0800

    tmp

--- myframe/core/base_timer_process.h
+++ myframe/core/base_timer_process.h
@@ -7,15 +7,15 @@ class base_timer_process
 {   
     public:
 
-    uint32_t _timer_type;
-    base_timer_process()
-    {   
-        _timer_type = 0;
-    }
+        uint32_t _timer_type;
+        base_timer_process()
+        {   
+            _timer_type = 0;
+        }
 
-    virtual ~base_timer_process();
+        virtual ~base_timer_process();
 
-    virtual void handle_timeout(const uint32_t timer_type);
+        virtual void handle_timeout(const uint32_t timer_type);
 };
 
 
