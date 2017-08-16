7bae46e9179cc42a19c6785c1f00e94876da237b
zm <zhangming025251@gmail.com>
2017-08-17 00:25:45 +0800

    tmp

--- myframe/kf_server/test/server.js
+++ myframe/kf_server/test/server.js
@@ -3,6 +3,6 @@ var WebSocketServer = require('/usr/local/node/lib/node_modules/ws/').Server
    wss.on('connection', function(ws) {
            ws.on('message', function(message) {
                        console.log('received: %s', message);
+                       ws.send('something');
                            });
-               ws.send('something');
    });
