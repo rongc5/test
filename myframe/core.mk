3649e4e94d0b0ff382ea02fea33ffc109cef0961
zm025251 <zhangming025251@gmail.com>
2017-10-10 19:13:17 +0800

    tmp

--- myframe/core.mk
+++ myframe/core.mk
@@ -44,7 +44,7 @@ LIBNAME=myframe
 all	: $(LIBNAME)
 
 clean	:
-	/bin/rm -rf  $(OBJ) lib$(LIBNAME)
+	/bin/rm -rf  $(OBJ) lib$(LIBNAME).a
 
 #=================================================
 $(LIBNAME):$(OBJ)
--- myframe/str2sign/core.mk
+++ myframe/str2sign/core.mk
@@ -20,7 +20,7 @@ LIBNAME=sign
 all	: $(LIBNAME)
 
 clean	:
-	/bin/rm -rf  $(OBJ) lib$(LIBNAME)
+	/bin/rm -rf  $(OBJ) lib$(LIBNAME).a
 
 #=================================================
 $(LIBNAME):$(OBJ)
