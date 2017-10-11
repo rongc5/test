87fa0845417c0bc507b18617d6aed4ce16aa1154
zm025251 <zhangming025251@gmail.com>
2017-10-11 19:20:07 +0800

    tmp

--- myframe/core/common_obj_container.cpp
+++ myframe/core/common_obj_container.cpp
@@ -7,56 +7,118 @@
 #include "log_helper.h"
 
 
-common_obj_container::common_obj_container(base_net_thread * thread):base_net_container(thread)
+common_obj_container::common_obj_container(base_net_thread * thread, uint32_t obj_num):base_net_container(thread)
 {
     base_net_container::init();
+
+    this->init(obj_num);
 }
+
+void common_obj_container::init(uint32_t obj_num)
+{
+    _obj_map = new(std::nothrow)inc_dict_t<base_net_obj *>();
+    if(_obj_map == NULL){
+        LOG_WARNING("new _obj_map failed");
+        return ;
+    }
+    if(_obj_map->create(obj_num/2, obj_num/3) < 0) {
+        delete _obj_map;
+        LOG_WARNING("failed to allocate memory for _obj_map");
+        return ;
+    }
+
+    _obj_net_map = new(std::nothrow)inc_dict_t<base_net_obj *>();
+    if(_obj_net_map == NULL){
+        LOG_WARNING("new _obj_net_map failed");
+        return ;
+    }
+    if(_obj_net_map->create(obj_num/2, obj_num/3) < 0) {
+        delete _obj_net_map;
+        LOG_WARNING("failed to allocate memory for _obj_net_map");
+        return ;
+    }
+}
+
 
 common_obj_container::~common_obj_container()
 {
+    base_net_obj ** p_data = NULL;
+    inc_dict_t<base_net_obj *>::travel_info_t m_tranverse;
+        memset(&m_tranverse, 0, sizeof(m_tranverse));
+
+    uint32_t sign[2];
+    for(p_data = _obj_map->get_begin(&m_tranverse, sign);
+            p_data != _obj_map->get_end(&m_tranverse);
+            p_data = _obj_map->get_next(&m_tranverse, sign)) {
+
+        (*p_data)->destroy();
+    }
+
+    if (_obj_net_map) {
+        delete _obj_net_map;
+        _obj_net_map = NULL;
+    }
+
+    if (_obj_map) {
+        delete _obj_map;
+        _obj_map = NULL;
+    }
 }
 
 void common_obj_container::push_real_net(base_net_obj *p_obj)
 {
-    _obj_net_map.insert(make_pair(p_obj->get_id(), p_obj));
+    uint32_t sign[2];
+    sign[0] = p_obj->get_id()._id;
+    sign[1] = 0;
+
+    _obj_net_map->add_node(sign, &p_obj);
 }
 
 
 void common_obj_container::insert(base_net_obj *p_obj)
 {
     p_obj->set_id(gen_id_str());
-    _obj_map.insert(make_pair(p_obj->get_id(), p_obj));
+    uint32_t sign[2];
+    sign[0] = p_obj->get_id()._id;
+    sign[1] = 0;
+
+    _obj_map->add_node(sign, &p_obj);
 }
 
 base_net_obj* common_obj_container::find(const ObjId * obj_id)
 {
-    base_net_obj *p_obj = NULL;
-    map<ObjId, base_net_obj*>::iterator itr =  _obj_map.find(*obj_id);
-    if (itr != _obj_map.end())
-        p_obj = itr->second;
-    return p_obj;
+    if (obj_id->_thread_index != get_net_thread()->get_thread_index()){
+        return NULL;
+    }
+
+    uint32_t sign[2] = {obj_id->_id, 0};
+    base_net_obj** p_obj = _obj_map->seek_node(sign);
+    if (!p_obj){
+        return NULL;
+    }else {
+        return *p_obj;
+    }
 }
 
 bool common_obj_container::erase(ObjId *obj_id)
 {
-    if (!obj_id){
-        return false;
-    }
-    bool ret = false;
     base_net_obj *p_obj = NULL;
-    map<ObjId, base_net_obj*>::iterator itr =  _obj_map.find(*obj_id);
-    if (itr != _obj_map.end())
-    {
-        p_obj = itr->second;
-        if (p_obj != NULL)
-        {
-            _obj_net_map.erase(p_obj->get_id());
-            _obj_map.erase(p_obj->get_id());
-            p_obj->destroy();
-        }
+    bool ret = false;
+    if (!obj_id){
+        return ret;
+    }
 
+    p_obj = find(obj_id);
+    if (p_obj != NULL)
+    {
+        uint32_t sign[2] = {obj_id->_id, 0};
+
+        _obj_net_map.remove_node(sign);
+        _obj_map.remove_node(sign);
+        p_obj->destroy();
         ret = true;
     }
+
     return ret;
 }
 
