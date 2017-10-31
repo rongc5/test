07a01cea42d02e6999286c69bf828b1bba23a70a
zm025251 <zhangming025251@gmail.com>
2017-10-31 18:55:27 +0800

    tmp

--- stock/py/stock_info_bak.py
+++ stock/py/stock_info_bak.py
@@ -592,7 +592,7 @@ def get_money_flow(id, req_header=None):
                     key = '%d_%s' % (index, id)
                     req_header.extend(['If-None-Match: %s' % (user_agent_dic[key])])
 
-                httpGetContent(favicon, ['Referer: %s' % (url), 'User-Agent: %s' % (user_agent_list[index])])
+                #httpGetContent(favicon, ['Referer: %s' % (url), 'User-Agent: %s' % (user_agent_list[index])])
                 res = httpGetContent(url, req_header)
                 value = res['body'].decode("gbk").split('=')[1].strip(';\r\n')
 
