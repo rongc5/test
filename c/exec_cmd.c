49024731af3cc6f7727c865dbeb922dec9cd12fb
zm025251 <zhangming025251@gmail.com>
2018-02-11 19:06:53 +0800

    tmp

---
+++ c/exec_cmd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    FILE * fp;
+    char buffer[80];
+    fp = popen("cat /etc/passwd","r");
+    fgets(buffer,sizeof(buffer),fp);
+    printf("%s",buffer);
+    pclose(fp);
+
+    return 0;
+}
