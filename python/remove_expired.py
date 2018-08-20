08a508e4058287f8cc2cf02fd8e7fe7aad36214e
zm025251 <zhangming025251@gmail.com>
2018-08-20 18:38:00 +0800

    tmp

--- python/remove_expired.py
+++ python/remove_expired.py
@@ -9,9 +9,14 @@ import os
 
 
 #dir:filed
-expired_info = [{'dir':'readcurrent', 'field':'readcurrent', 'expired': -6,
+#expired_info = [{'dir':'readcurrent', 'field':'readcurrent', 'expired': -7,
+ #                'tr':'/data/mingz/bin/tr5',
+ #                'redis2_ns':'10.26.24.66:7000', 'redis_tp':'1'}]
+
+expired_info = [{'dir':'user_type', 'field':'user_type', 'expired': -1,
                  'tr':'/data/mingz/bin/tr5',
-                 'redis2_ns':'10.26.24.66:7000', 'redis_tp':'1'}]
+                 'redis2_ns':'10.26.24.66:7000', 'redis_tp':'1'}, {'dir':'readcurrent', 'field':'readcurrent', 'expired': -7,
+				'tr':'/data/mingz/bin/tr5','redis2_ns':'10.26.24.66:7000', 'redis_tp':'1'}]
 base_dir = '/data/mingz/rs9'
 
 class Day():
@@ -180,51 +185,75 @@ class Day():
      return "-".join("%s" % i for i in arr)
 
 
+def load_saved_dict(dic, base_path, dst_dic):
+    if not len(dic):
+        return
+
+    path_dic = []
+    day = Day()
+    lastday = 0
+    for id_day in range(abs(dic['expired'])):
+        date = ''
+
+        date =  '%s' % (day.get_day_of_day(lastday), )
+        path = '%s/%s/%s'%(base_path, date, dic['dir'])
+        path_dic.append(path)
+
+        lastday = lastday - 1
+
+    print path_dic
+    for key in path_dic:
+        files = os.listdir(key)
+        for file in files:
+            #print file
+            if not os.path.isdir(file) and file.startswith('data_'):
+                if key in file:
+                    f = open(file)
+                else:
+                    f = open(key+"/"+file)
+
+                while 1:
+                    line = f.readline().strip()
+                    if not line:
+                        break
+                    items = line.split('\t')
+                    if len(items):
+                        dst_dic[items[0]] = ''
+                        #print items[0]
+                f.close()
+
 def do_remove_expired(dic, base_path):
     if not len(dic):
         return
 
     day = Day()
     start_day = day.get_day_of_day(dic['expired'])
-    print start_day
     path = '%s/%s/%s'%(base_path, start_day, dic['dir'])
     if not os.path.isdir(path):
         print path, 'not exist'
         return
 
     id_dic = {}
-    files = os.listdir(path)
-    for file in files:
-        if not os.path.isdir(file):
-            if path in file:
-                f = open(file)
-            else:
-                f = open(path+"/"+file)
+    load_saved_dict(dic, base_path, id_dic)
+    if not len(id_dic):
+        return
+    
+    index = dic['expired']
 
-            while 1:
-                line = f.readline().strip()
-                if not line:
-                    break
-                items = line.split('\t')
-                if len(items):
-                    id_dic[items[0]] = items[0]
-                    #print items[0]
-            f.close()
-
-    for index in range(abs(dic['expired'])):
-        remove_list = []
-        if index > 0:
-			index = index * -1
+    while 1:
         start_day = day.get_day_of_day(index)
-        print start_day
         path = '%s/%s/%s'%(base_path, start_day, dic['dir'])
+        print path
+
         if not os.path.isdir(path):
             print path, 'not exist'
-            continue
+            break
 
+        index -= 1
         files = os.listdir(path)
+        fp = open('/data/mingz/base_list', "w")
         for file in files:
-            if not os.path.isdir(file):
+            if not os.path.isdir(file) and file.startswith('data_'):
                 if path in file:
                     f = open(file)
                 else:
@@ -235,23 +264,22 @@ def do_remove_expired(dic, base_path):
                     if not line:
                         break
                     items = line.split('\t')
-                    if len(items) and items[0] in id_dic:
-                        remove_list.append(items[0])
+                    if len(items) and items[0] not in id_dic:
+                        fp.write('hdel c%s %s' % (items[0], dic['field']))
+                        fp.write('\n')
+                        #file.write('hdel c%s %s' % (key, 'readhistory'))
+                        #file.write('\n')
+                        fp.flush()
                 f.close()
+		
+        fp.close()
 
-        for key in remove_list:
-            id_dic.pop(key)
-
-    file = open('base_list', "w+")
-    for key in id_dic:
-        file.write('hdel c%s %s' % (key, dic['field']))
-        file.write('\n')
-    file.close()
+        cmd = '%s -s %s -p %s -c %s' % (dic['tr'], dic['redis2_ns'], 'base_list', dic['redis_tp'])
+        print cmd
+        res = os.popen(cmd).read()
+        print res
     print len(id_dic)
 
-    cmd = '%s -s %s -p %s -c %s' % (dic['tr'], dic['redis2_ns'], 'base_list', dic['redis_tp'])
-    res = os.popen(cmd).read()
-    print res
 
 
 if __name__ == '__main__':
