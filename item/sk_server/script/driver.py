44e8cebd1141d702c4ce63a3ee422f85505af6ac
zhangming <zhangming025251@gmail.com>
2020-05-17 17:55:02 +0800

    add som st

---
+++ item/sk_server/script/driver.py
@@ -0,0 +1,345 @@
+#!/usr/bin/env python
+#encoding=utf-8
+
+import json
+import time
+import pycurl
+import cStringIO
+import gzip
+import unicodedata
+import os
+import sys
+import random
+import subprocess
+import ast
+import json
+
+from time import strftime, localtime
+from datetime import timedelta, date
+import calendar
+
+import ConfigParser
+
+import optparse
+
+__author__ = 'rong'
+
+MAX_RESPONSE_KB = 100*1024
+
+user_agent_list = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+        "(KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 "
+        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 "
+        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+
+def log_write(filename, str):
+    file = open(filename, 'a')
+    file.write(str)
+    file.write('\n')
+    file.flush()
+    file.close()
+
+class HTTPError(Exception):
+    """Exception that is wrapped around all exceptions that are raised
+    by the underlying fetcher when using the ExceptionWrappingFetcher
+
+    @ivar why: The exception that caused this exception
+    """
+    def __init__(self, why=None):
+        Exception.__init__(self, why)
+        self.why = why
+
+class CurlHTTPFetcher(object):
+    """
+    An C{L{HTTPFetcher}} that uses pycurl for fetching.
+    See U{http://pycurl.sourceforge.net/}.
+    """
+    ALLOWED_TIME = 30 # seconds
+
+    def _parseHeaders(self, header_file):
+        header_file.seek(0)
+
+        # Remove the status line from the beginning of the input
+        unused_http_status_line = header_file.readline().lower ()
+        if unused_http_status_line.startswith('http/1.1 100 '):
+            unused_http_status_line = header_file.readline()
+            unused_http_status_line = header_file.readline()
+
+        lines = [line.strip() for line in header_file]
+
+        # and the blank line from the end
+        empty_line = lines.pop()
+        if empty_line:
+            raise HTTPError("No blank line at end of headers: %r" % (line,))
+
+        headers = {}
+        for line in lines:
+            try:
+                name, value = line.split(':', 1)
+            except ValueError:
+                raise HTTPError(
+                    "Malformed HTTP header line in response: %r" % (line,))
+
+            value = value.strip()
+
+            # HTTP headers are case-insensitive
+            name = name.lower()
+            headers[name] = value
+
+        return headers
+
+    def fetch(self, url, body=None, headers=None):
+        stop = int(time.time()) + self.ALLOWED_TIME
+        off = self.ALLOWED_TIME
+
+        if headers is None:
+            headers = {}
+
+        if  'User-Agent:' in headers or  'user-agent:' in headers:
+            print 'no User-Agent', headers, url, sys._getframe().f_lineno
+            return
+
+        header_list = []
+        if headers is not None:
+            for header_name, header_value in headers.iteritems():
+                header_list.append('%s: %s' % (header_name, header_value))
+
+        c = pycurl.Curl()
+        try:
+            c.setopt(pycurl.NOSIGNAL, 1)
+
+            if header_list:
+                c.setopt(pycurl.HTTPHEADER, header_list)
+
+            # Presence of a body indicates that we should do a POST
+            if body is not None:
+                c.setopt(pycurl.POST, 1)
+                c.setopt(pycurl.POSTFIELDS, body)
+
+            while off > 0:
+                if not url.startswith('http://') and not url.startswith('https://'):
+                    raise HTTPError("Fetching URL not allowed: %r" % (url,))
+
+                data = cStringIO.StringIO()
+                def write_data(chunk):
+                    if data.tell() > 1024*MAX_RESPONSE_KB:
+                        return 0
+                    else:
+                        #print 'hello', data.getvalue().decode('gbk')
+                        return data.write(chunk)
+
+                response_header_data = cStringIO.StringIO()
+                c.setopt(pycurl.WRITEFUNCTION, write_data)
+                c.setopt(pycurl.HEADERFUNCTION, response_header_data.write)
+                c.setopt(pycurl.TIMEOUT, off)
+                c.setopt(pycurl.URL, url)
+
+                c.perform()
+
+                response_headers = self._parseHeaders(response_header_data)
+                code = c.getinfo(pycurl.RESPONSE_CODE)
+                if code in [301, 302, 303, 307]:
+                    url = response_headers.get('location')
+                    if url is None:
+                        raise HTTPError(
+                            'Redirect (%s) returned without a location' % code)
+
+                    # Redirects are always GETs
+                    c.setopt(pycurl.POST, 0)
+
+                    # There is no way to reset POSTFIELDS to empty and
+                    # reuse the connection, but we only use it once.
+                else:
+                    res = {}
+                    res['head'] = response_headers
+                    res['status'] = code
+                    res['final_url'] = url
+                    res['body'] = '%s' % (data.getvalue())
+                    return res
+
+                off = stop - int(time.time())
+
+            raise HTTPError("Timed out fetching: %r" % (url,))
+        finally:
+            response_header_data.close()
+            data.close()
+            c.close()
+
+def print_res(res, parser):
+    #if res.has_key('recode'):
+        #print res['recode']
+
+    if res.has_key('data'):
+        for item in res['data']:
+            key_list = []
+            for key in item:
+                if 'block' in key:
+                    continue
+                key_list.append(key)
+            key_list.sort()
+            for key in key_list:
+                tmp_str = key + '='
+                myjson = json.dumps(item[key], ensure_ascii=False)
+                tmp_str += myjson
+                if not parser.outfile:
+                    print tmp_str
+                if parser.outfile:
+                    log_write(parser.outfile, tmp_str.encode('gb18030'))
+
+
+
+            if parser.outfile:
+                log_write(parser.outfile, '\n')
+            else:
+                print '\n'
+
+
+def do_query_id(parser):
+    url = 'http://%s:%s/queryid?id=%s' % (parser.ip, parser.port, parser.id)
+
+    if parser.history_num:
+        url += '&history_num=%d' % (parser.history_num)
+
+    if parser.history_date:
+        url += '&history_date=%s' % (parser.history_date)
+    if parser.single_in:
+        url += '&single_in=%d' % (parser.single_in)
+
+    if parser.single_out:
+        url += '&single_out=%d' % (parser.single_out)
+    print url
+
+    header = {}
+    index = random.randint(0, len(user_agent_list) -1)
+    header['User-Agent'] = user_agent_list[index]
+
+    res = {}
+    try:
+        curl =  CurlHTTPFetcher()
+        curl.ALLOWED_TIME = 2
+        res = curl.fetch(url, None, header)
+    except BaseException, e:
+        print e.message
+
+    if not res.has_key('body'):
+        print 'net not ok'
+        return
+    res_str = res['body'].strip()
+    res_str = res_str.decode('gb18030')
+    #res_str = json.loads(res_str, encoding="GB2312")
+    res_str = json.loads(res_str)
+    if res_str:
+        print_res(res_str, parser)
+
+
+def do_select(parser):
+    if not os.path.isfile(parser.ini):
+        print 'slect ini file not exist'
+
+    url = 'http://%s:%s/select?' % (parser.ip, parser.port)
+
+    body = ''
+    file = open(parser.ini, 'r')
+    for line in file.readlines():
+        line = line.strip()
+        if not len(line) or line.startswith('#'):
+            continue;
+        url += line
+        url += '&'
+
+    url = url.strip('&')
+    print url
+
+    header = {}
+    index = random.randint(0, len(user_agent_list) -1)
+    header['User-Agent'] = user_agent_list[index]
+    res = {}
+
+    try:
+        curl =  CurlHTTPFetcher()
+        curl.ALLOWED_TIME = 2
+        res = curl.fetch(url, body, header)
+    except BaseException, e:
+        print e.message
+
+    if not res.has_key('body'):
+        print 'net not ok'
+        return
+
+    res_str = res['body'].strip()
+    res_str = res_str.decode('gb18030')
+    res_str = json.loads(res_str)
+    if res_str:
+        #print res_str
+        print_res(res_str, parser)
+
+
+def do_search():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--time",
+                  action="store", dest="time", type="int",default=0,
+              help="http req interval second")
+
+    parser.add_option("-i", "--ip",
+                  action="store", dest="ip",type="string", default="127.0.0.1",
+              help="http server ip")
+
+    parser.add_option("-p", "--port",
+                  action="store", dest="port", type="string", default="8088",
+              help="http server port")
+
+    parser.add_option("-I", "--id",
+                  action="store", dest="id",type="string",default="",
+              help="stock id like sh601318")
+
+    parser.add_option("-D", "--detail_in",
+                  action="store", dest="single_in",type="int",default=0,
+              help="show the single in")
+
+    parser.add_option("-O", "--detail_out",
+                  action="store", dest="single_out",type="int",default=0,
+              help="show the single out")
+
+    parser.add_option("-n", "--history_num",
+                  action="store", dest="history_num",type="int",default=0,
+              help="last trade days history_num")
+
+    parser.add_option("-d", "--history_date",
+                  action="store", dest="history_date", type="string", default="",
+              help="http history_date")
+
+    parser.add_option("-o", "--outfile",
+                  action="store", dest="outfile",type="string",default="",
+              help="log write res to file")
+
+    parser.add_option("-f", "--ini",
+                  action="store", dest="ini",type="string",default="",
+              help="select ini file")
+
+
+    options, args = parser.parse_args()
+    #print options
+    #print 'Query string:', options.time
+
+    while 1:
+        if options.id:
+            do_query_id(options)
+
+        if options.ini:
+            do_select(options)
+
+        if options.time:
+            time.sleep(options.time)
+        else:
+            break
+    #parser.print_help()
+
+if __name__ == '__main__':
+    do_search()
