106b2eaf6978de53281f4c966c3ed3ecb0af3d44
zhangming <zhangming025251@gmail.com>
2020-10-23 18:02:04 +0800

    add crontab script

--- item/sk_server/tools/load.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /etc/profile
-python /data2/mingz/.stock/script/load_ids.py
-python /data2/mingz/.stock/script/load_finance.py
-python /data2/mingz/.stock/script/load_brief.py
-python /data2/mingz/.stock/script/load_plate.py
-#python load_plate.py
